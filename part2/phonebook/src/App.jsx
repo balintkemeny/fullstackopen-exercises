ca8683998368fd623a65a54db52e12002bec45ff
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-01-23 16:37:56 +0100

    ex2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleInputNewName = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  };
+
+  const handleClickAddPerson = (event) => {
+    event.preventDefault();
+
+    const newPerson = { name: newName };
+    setPersons(persons.concat(newPerson));
+    setNewName("");
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <div>
+          name: <input value={newName} onChange={handleInputNewName} />
+        </div>
+        <div>
+          <button type="submit" onClick={handleClickAddPerson}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) => (
+          <div key={person.name}>{person.name}</div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
