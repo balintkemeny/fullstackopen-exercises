087e2d01169c74d993d11b156e18c7e29f57d68a
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-02-08 11:31:29 +0100

    ex3.4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-const persons = [
+let persons = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -38,6 +38,13 @@ app.get("/api/persons/:id", (req, res) => {
   }
 
   res.json(person);
+});
+
+app.delete("/api/persons/:id", (req, res) => {
+  const id = req.params.id;
+  persons = persons.filter((p) => p.id !== id);
+
+  res.status(204).end();
 });
 
 app.get("/info", (req, res) => {
