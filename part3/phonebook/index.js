b2fb0bd496bb6e9f8093c59127032fde3f8707dd
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-03-02 14:02:10 +0100

    ex3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -90,11 +90,12 @@ app.get("/api/persons/:id", (req, res) => {
   res.json(person);
 });
 
-app.delete("/api/persons/:id", (req, res) => {
-  const id = req.params.id;
-  persons = persons.filter((p) => p.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => next(error));
 });
 
 app.get("/info", (req, res) => {
