bb4bdbbff3e9ffeea434d0d3ef87dc808408cd4a
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-03-02 14:38:04 +0100

    remove unnecessary hardcoded initial data

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -4,29 +4,6 @@ const Person = require("./models/person");
 
 const app = express();
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.use(express.json());
 
 morgan.token("requestBody", (req, res) => JSON.stringify(req.body));
