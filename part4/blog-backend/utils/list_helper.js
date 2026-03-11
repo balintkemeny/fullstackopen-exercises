84109b7386466172965371109973890c1eed8644
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-03-11 14:34:55 +0100

    ex4.5-ex4.6

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -6,4 +6,48 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, elem) => acc + elem.likes, 0);
 };
 
-module.exports = { dummy, totalLikes };
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const likes = blogs.map((elem) => elem.likes);
+  const maxIndex = likes.indexOf(Math.max(...likes));
+
+  return blogs[maxIndex];
+};
+
+const getAuthorsBlogCount = (blogs) => {
+  const authorsMap = new Map();
+  blogs.forEach((elem) => {
+    if (!authorsMap.has(elem.author)) {
+      authorsMap.set(elem.author, 1);
+    } else {
+      authorsMap.set(elem.author, authorsMap.get(elem.author) + 1);
+    }
+  });
+
+  const authorsArray = [];
+  authorsMap.forEach((val, key) => {
+    authorsArray.push({
+      author: key,
+      blogs: val,
+    });
+  });
+
+  return authorsArray;
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+
+  const authors = getAuthorsBlogCount(blogs);
+  const blogsCount = authors.map((elem) => elem.blogs);
+  const maxIndex = blogsCount.indexOf(Math.max(...blogsCount));
+
+  return authors[maxIndex];
+};
+
+module.exports = { dummy, totalLikes, favouriteBlog, mostBlogs };
