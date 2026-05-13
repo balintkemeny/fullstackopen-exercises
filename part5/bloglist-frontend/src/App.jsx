987102fdf176bba069cd0d1f1234bf4cd7da6a20
Balint Kemeny <balint.kemeny@dkfz-heidelberg.de>
2026-05-13 11:32:37 +0200

    ex5.1: add login functionality

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,24 +1,59 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import LoginForm from "./components/LoginForm";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
+  const [blogs, setBlogs] = useState([]);
+  const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log("logging in with:", username, password);
+
+    try {
+      const user = await loginService.login({ username, password });
+      console.log("user:", user);
+      setUser(user);
+      setUsername("");
+      setPassword("");
+    } catch (error) {
+      console.log("login error:", error);
+    }
+  };
 
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+      {!user && (
+        <>
+          <h2>log in to application</h2>
+          <LoginForm
+            handleLogin={handleLogin}
+            username={username}
+            setUsername={setUsername}
+            password={password}
+            setPassword={setPassword}
+          />
+        </>
+      )}
+      {user && (
+        <>
+          <h2>blogs</h2>
+          <p>{user.name} logged in</p>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
