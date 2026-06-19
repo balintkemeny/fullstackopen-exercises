f32108004e3a489ee16a1c39a37ab17b6cc36581
Bálint Kemény <balintkemeny@gmail.com>
2026-06-19 16:25:49 +0200

    ex5.13-ex5.14

---
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -0,0 +1,75 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import Blog from "./Blog";
+
+const testBlogFull = {
+  title: "test title",
+  author: "test author",
+  url: "www.example.test",
+  likes: 1,
+  user: {
+    username: "testuser",
+    name: "Test User",
+  },
+};
+
+test("renders title", () => {
+  render(<Blog blog={testBlogFull} />);
+
+  const element = screen.getByText(testBlogFull.title, { exact: false });
+  expect(element).toBeDefined();
+});
+
+test("renders author", () => {
+  render(<Blog blog={testBlogFull} />);
+
+  const element = screen.getByText(testBlogFull.author, { exact: false });
+  expect(element).toBeDefined();
+});
+
+test("does not render url by default", () => {
+  render(<Blog blog={testBlogFull} />);
+
+  const element = screen.queryByText(testBlogFull.url, { exact: false });
+  expect(element).toBeNull();
+});
+
+test("does not likes url by default", () => {
+  render(<Blog blog={testBlogFull} />);
+
+  const element = screen.queryByText(`likes: ${testBlogFull.likes}`, {
+    exact: false,
+  });
+  expect(element).toBeNull();
+});
+
+test("clicking the show button displays the URL and the number of likes as well", async () => {
+  render(<Blog blog={testBlogFull} />);
+  const user = userEvent.setup();
+
+  const button = screen.getByText("show");
+  await user.click(button);
+
+  const urlDiv = screen.getByText(testBlogFull.url);
+  expect(urlDiv).toBeDefined();
+
+  const likesDiv = screen.getByText(`likes: ${testBlogFull.likes}`);
+  expect(likesDiv).toBeDefined();
+});
+
+test("clicking the hide button hides the URL and the number of likes as well", async () => {
+  render(<Blog blog={testBlogFull} />);
+  const user = userEvent.setup();
+
+  const showButton = screen.getByText("show");
+  await user.click(showButton);
+
+  const hideButton = screen.getByText("hide");
+  await user.click(hideButton);
+
+  const urlDiv = screen.queryByText(testBlogFull.url);
+  expect(urlDiv).toBeNull();
+
+  const likesDiv = screen.queryByText(`likes: ${testBlogFull.likes}`);
+  expect(likesDiv).toBeNull();
+});
