206f87e5a33faa743b212732eed194c941aa1811
Bálint Kemény <balintkemeny@gmail.com>
2026-07-08 16:57:50 +0200

    ex5.18

--- part5/bloglist-e2e/tests/bloglist.spec.js
+++ part5/bloglist-e2e/tests/bloglist.spec.js
@@ -1,4 +1,5 @@
 const { beforeEach, test, describe, expect } = require("@playwright/test");
+const { loginWith } = require("./helper");
 
 describe("Bloglist app", () => {
   const user = {
@@ -20,5 +21,23 @@ describe("Bloglist app", () => {
     await expect(page.getByLabel("username")).toBeVisible();
     await expect(page.getByLabel("password")).toBeVisible();
     await expect(page.getByRole("button", { name: "login" })).toBeVisible();
+  });
+
+  test("Login succeeds when correct credentials are provided", async ({
+    page,
+  }) => {
+    await loginWith(page, user.username, user.password);
+    await expect(page.getByText("Test User logged in")).toBeVisible();
+  });
+
+  test("Login fails when wrong credentials are provided", async ({ page }) => {
+    await loginWith(page, user.username, "wrong_pwd");
+
+    const errorDiv = page.locator(".error");
+    await expect(errorDiv).toContainText("wrong username or password");
+    await expect(errorDiv).toHaveCSS("border-style", "solid");
+    await expect(errorDiv).toHaveCSS("color", "rgb(255, 0, 0)");
+
+    await expect(page.getByText("Test User logged in")).not.toBeVisible();
   });
 });
