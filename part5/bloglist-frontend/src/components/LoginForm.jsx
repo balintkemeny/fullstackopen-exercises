6e49b48c4ff32d07a96d67a260c69d162099e145
Bálint Kemény <balintkemeny@gmail.com>
2026-08-07 14:53:56 +0200

    ex5.24: with redirect

--- part5/bloglist-frontend/src/components/LoginForm.jsx
+++ part5/bloglist-frontend/src/components/LoginForm.jsx
@@ -1,33 +1,44 @@
+import { useNavigate } from "react-router-dom";
+
 const LoginForm = ({
   handleLogin,
   username,
   setUsername,
   password,
   setPassword,
-}) => (
-  <form onSubmit={handleLogin}>
-    <div>
-      <label>
-        username
-        <input
-          type="text"
-          value={username}
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </label>
-    </div>
-    <div>
-      <label>
-        password
-        <input
-          type="password"
-          value={password}
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </label>
-    </div>
-    <button type="submit">login</button>
-  </form>
-);
+}) => {
+  const navigate = useNavigate();
+
+  return (
+    <form
+      onSubmit={(event) => {
+        handleLogin(event);
+        navigate("/");
+      }}
+    >
+      <div>
+        <label>
+          username
+          <input
+            type="text"
+            value={username}
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          password
+          <input
+            type="password"
+            value={password}
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </label>
+      </div>
+      <button type="submit">login</button>
+    </form>
+  );
+};
 
 export default LoginForm;
