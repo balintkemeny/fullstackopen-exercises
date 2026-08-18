9f760e36670fcce39bf2e92a472bd00badb7cb9b
Bálint Kemény <balintkemeny@gmail.com>
2026-08-18 15:36:06 +0200

    ex5.31: implement material ui styling for BlogDetails

--- part5/bloglist-frontend/src/components/BlogDetails.jsx
+++ part5/bloglist-frontend/src/components/BlogDetails.jsx
@@ -51,12 +51,34 @@ const BlogDetails = ({ blog, updateBlog, deleteBlog, currentUsername }) => {
           Added by {blog.user.name}
         </Typography>
         <div>
-          likes: {blog.likes}{" "}
-          {currentUsername && <button onClick={handleClickLike}>like</button>}
+          <Typography
+            variant="body1"
+            component="span"
+            sx={{ display: "inline" }}
+          >
+            likes: {blog.likes}{" "}
+          </Typography>
+          {currentUsername && (
+            <Button
+              variant="outlined"
+              color="success"
+              onClick={handleClickLike}
+              sx={{ marginLeft: 0.5, marginRight: 0.5, display: "inline" }}
+            >
+              like
+            </Button>
+          )}
+          {blog.user.username === currentUsername && (
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={handleClickRemove}
+              sx={{ marginLeft: 0.5, display: "inline" }}
+            >
+              remove
+            </Button>
+          )}
         </div>
-        {blog.user.username === currentUsername && (
-          <button onClick={handleClickRemove}>remove</button>
-        )}
       </CardContent>
     </Card>
   );
